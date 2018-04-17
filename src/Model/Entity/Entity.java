ace25c396c4f0352dbc174d462b8ecf6f6aaf6f3
Nikita Buslov <buslovna@ufl.edu>
2018-04-16 21:47:52 -0400

    controller stuff

--- src/Model/Entity/Entity.java
+++ src/Model/Entity/Entity.java
@@ -1,4 +1,14 @@
 package Model.Entity;
 
+
 public class Entity {
+    private Orientation orientation;
+
+    Entity(){
+
+    }
+
+    public Orientation getOrientation() { return orientation;     }
+    void setOrientation(Orientation orientation){ this.orientation = orientation;     }
+
 }
