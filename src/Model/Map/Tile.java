3729092d811c55db722bc99dbbbfe3e55eb02aa8
Carlo Romo <delebot@gmail.com>
2018-04-16 21:33:35 -0400

    Start on map and tile

--- src/Model/Map/Tile.java
+++ src/Model/Map/Tile.java
@@ -1,4 +1,44 @@
 package Model.Map;
 
+import Model.Entity.Entity;
+import Model.Map.Terrain.Terrain;
+import Model.Item.Item;
+
 public class Tile {
+    private boolean isPassable;
+    private Terrain terrain;
+    private Item item;
+    private boolean visited;
+    private boolean inView;
+    private Decal decal;
+
+    public Tile() {
+        isPassable = true;
+        terrain = new Terrain();
+        decal = new Decal();
+        visited = false;
+        inView = false;
+    }
+
+    public Tile(Terrain t, Decal d, Item i) {
+        item = i;
+        terrain = t;
+        decal = d;
+    }
+
+    public void enter(Entity e) {
+
+    }
+    public void exit(Entity e) {
+
+    }
+    public boolean canEnter(Entity e) {
+        return isPassable;
+    }
+    public void detach(View v) {
+
+    }
+    public void attach(View v) {
+
+    }
 }
