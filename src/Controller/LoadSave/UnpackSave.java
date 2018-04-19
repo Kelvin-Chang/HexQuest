f3c15d75773977506d22bc1dca20a4cb51db9c23
Keanu Kerr <keanukerr@gmail.com>
2018-04-19 18:31:39 -0400

    Fixed some things with loading

--- src/Controller/LoadSave/UnpackSave.java
+++ src/Controller/LoadSave/UnpackSave.java
@@ -1,51 +1,51 @@
-//package Controller.LoadSave;
-//
-//import Model.Entity.Character.Player;
-//import View.Viewport;
-//import org.json.JSONArray;
-//import org.json.JSONObject;
-//
-//public class UnpackSave {
-//
-//    private GameBuilder gameBuilder;
-//
-//    private Player player;
-//
-//    private Viewport viewport = new Viewport();
-//
-//    public UnpackSave(JSONObject jso, GameBuilder gb) {
-//        this.gameBuilder = gb;
-//
-//        unpackWorld(jso.getJSONObject("World"));
-//    }
-//
-//    public void unpackWorld(JSONObject world) {
-//        System.out.println("Unpacking World: " + world);
-//
-//        gameBuilder.initWorld(world.getString("currentMap"));
-//
-//        JSONArray mapArray = world.getJSONArray("Map");
-//
-//        for(int i = 0; i < mapArray.length(); i++) {
-//            unpackMap(mapArray.getJSONObject(i));
-//        }
-//    }
-//
-//    public void unpackMap(JSONObject map) {
-//        System.out.println("Unpacking map: " + map);
-//
-//        JSONArray tileArray = map.getJSONArray("Tile");
-//
-//        gameBuilder.initZone(map.getString("id"), map.getInt("rows"), map.getInt("columns"));
-//
-//        for (int i = 0; i < tileArray.length(); i++) {
-//            unpackTile(tileArray.getJSONObject(i), map.getString("id"));
-//        }
-//    }
-//
-//    public void unpackTile(JSONObject tile, String id) {
-//        System.out.println("Unpacking tile: " + tile);
-//        gameBuilder.initTile(tile.getString("terrain"), tile.getString("areaEffect"), tile.getString("decal"), tile.getString("item"), tile.getInt("x"), tile.getInt("y"), id);
-//    }
-//
-//}
+package Controller.LoadSave;
+
+import Model.Entity.Character.Player;
+import View.Viewport;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+public class UnpackSave {
+
+    private GameBuilder gameBuilder;
+
+    private Player player;
+
+    private Viewport viewport = new Viewport();
+
+    public UnpackSave(JSONObject jso, GameBuilder gb) {
+        this.gameBuilder = gb;
+
+        unpackWorld(jso.getJSONObject("World"));
+    }
+
+    public void unpackWorld(JSONObject world) {
+        System.out.println("Unpacking World: " + world);
+
+        gameBuilder.initWorld(world.getInt("currentMap"));
+
+        JSONArray mapArray = world.getJSONArray("Map");
+
+        for(int i = 0; i < mapArray.length(); i++) {
+            unpackMap(mapArray.getJSONObject(i));
+        }
+    }
+
+    public void unpackMap(JSONObject map) {
+        System.out.println("Unpacking map: " + map);
+
+        JSONArray tileArray = map.getJSONArray("Tile");
+
+        gameBuilder.initZone(map.getInt("id"), map.getInt("rows"), map.getInt("columns"));
+
+        for (int i = 0; i < tileArray.length(); i++) {
+            unpackTile(tileArray.getJSONObject(i), map.getInt("id"));
+        }
+    }
+
+    public void unpackTile(JSONObject tile, Integer id) {
+        System.out.println("Unpacking tile: " + tile);
+        gameBuilder.initTile(tile.getString("terrain"), tile.getString("areaEffect"), tile.getString("decal"), tile.getString("item"), tile.getInt("x"), tile.getInt("y"), id);
+    }
+
+}
