37df887c06c843c67ea39910d5157e4b4d34a96c
Kelvin-Chang <kelvinchang36@gmail.com>
2018-04-18 21:40:24 -0400

    need to figure out why this breaks

--- src/View/Menu/NewGameView.java
+++ src/View/Menu/NewGameView.java
@@ -1,30 +1,100 @@
 package View.Menu;
 
-import View.buttons.Selectable;
-import View.buttons.MainMenuSelectable;
-import View.buttons.StartNewGameSelectable;
+import View.buttons.*;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Button;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 
 
 public class NewGameView extends AbstractView{
 
+    private ViewController viewController;
+
     public NewGameView() {
 
         ViewController viewController = new ViewController();
-        GridPane grid = new GridPane();
 
-        grid.setVgap(10);
-        grid.setHgap(10);
-        grid.setPadding(new Insets(10,10,10,10));
+
+//        GridPane grid = new GridPane();
+//
+//        grid.setVgap(10);
+//        grid.setHgap(10);
+//        grid.setPadding(new Insets(10,10,10,10));
+//
+//        ArrayList<Selectable> options = new ArrayList<Selectable>() {{
+//            add(new StartNewGameSelectable("Start", viewController));
+//            add(new MainMenuSelectable("Main Menu", viewController));
+//        }};
+//
+//        for(Selectable clickable: options) {
+//            Button selectable = new Button(clickable.getName());
+//
+//            // sets selectable style
+//            selectable.getStyleClass().add("button1");
+//
+//            selectable.setOnAction(clickable);
+//            grid.add(selectable, 5, options.indexOf(clickable));
+//        }
+
+        this.getChildren().add(borderPane());
+    }
+
+    // list of buttons
+    private ArrayList<Selectable> leftPaneButtons(ViewController viewController) {
 
         ArrayList<Selectable> options = new ArrayList<Selectable>() {{
             add(new StartNewGameSelectable("Start", viewController));
             add(new MainMenuSelectable("Main Menu", viewController));
         }};
+
+        return options;
+    }
+
+    // creates vbox to hold buttons
+    private StackPane centerPane() {
+        StackPane stackPane = new StackPane();
+        stackPane.setMaxWidth(0);
+
+        return stackPane;
+    }
+
+
+//    private Text topPaneText() {
+//        Text t = new Text();
+//        t.setText("Game Title");
+//        t.setFont(Font.font("Elephant", 50));
+//        t.setFill(Paint.valueOf("#ff00ff"));
+//
+//        return t;
+//    }
+
+
+    private StackPane topPane() {
+        StackPane stackPane = new StackPane();
+        stackPane.setPrefSize(1000,300);
+        stackPane.setAlignment(Pos.BOTTOM_CENTER);
+//        stackPane.getChildren().add(topPaneText());
+
+        return stackPane;
+    }
+
+    // following panes set the panes to size 0 so the borderpane is essentially two panes stacked on top of each other
+    private VBox leftPane() {
+        ArrayList<Selectable> options = leftPaneButtons(viewController);
+        VBox vbox = new VBox();
+//        vbox.setPadding(new Insets(10,10,10,10));
+        vbox.setSpacing(10);
+        vbox.setPrefHeight(500);
+        vbox.setAlignment(Pos.TOP_CENTER);
 
         for(Selectable clickable: options) {
             Button selectable = new Button(clickable.getName());
@@ -32,10 +102,42 @@ public class NewGameView extends AbstractView{
             // sets selectable style
             selectable.getStyleClass().add("button1");
 
+            // set action
             selectable.setOnAction(clickable);
-            grid.add(selectable, 5, options.indexOf(clickable));
+
+            // add to vbox
+            vbox.getChildren().add(selectable);
         }
 
-        this.getChildren().add(grid);
+        return vbox;
+    }
+
+    private StackPane rightPane() {
+        StackPane stackPane = new StackPane();
+        stackPane.setMaxWidth(0);
+
+        return stackPane;
+    }
+
+    private StackPane bottomPane() {
+        StackPane stackPane = new StackPane();
+        stackPane.setMaxHeight(0);
+
+        return stackPane;
+    }
+
+    private BorderPane borderPane() {
+
+        // create new borderpane for formatting
+        BorderPane bp = new BorderPane();
+
+        bp.setTop(topPane());
+        bp.setCenter(centerPane());
+
+        bp.setBottom(bottomPane());
+        bp.setLeft(leftPane());
+        bp.setRight(rightPane());
+
+        return bp;
     }
 }
