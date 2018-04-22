d250a1874753d1162673a077e6f1b60695f4d1f8
Lorenzo Tabares <10tabareslorenzo@gmail.com>
2018-04-21 23:51:18 -0400

    MapGenerator

--- src/Controller/RunGame.java
+++ src/Controller/RunGame.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 
 public class RunGame {
     public static void main(String[] args) {
+//        MapGenerator map = new MapGenerator("a.json", 15, 15, 1);
         Application.launch(GameMediator.class, args);
         GameMediator gameMediator = new GameMediator();
     }
